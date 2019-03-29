f0864eb0b5575c1a038dcad44734a8422ee32afa
Ali Can Gönüllü <alicangonullu1907@gmail.com>
2019-03-29 15:50:42 +0300

    AliPlayer P2P Demo
    AliPlayer P2P Demo

---
+++ index.php
@@ -0,0 +1,98 @@
+
+<!DOCTYPE html>
+<html lang="tr">
+
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<meta name="theme-color" content="#bf1717">
+
+	<meta property="og:image" content="http://ferrolho.github.iohttps://veri.alicangonullu.biz/p2p/assets/thumbnail.png">
+
+	<link rel="icon" href="https://veri.alicangonullu.biz/p2p/favicon.png" type="image/png">
+
+	<title>P2P Sistemi Denemesi</title>
+	<meta name="description" content="P2P Müzik Sistemi Denemesi">
+
+	<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Asap:400,700">
+
+	<!-- GitHub Corner - https://github.com/simonwhitaker/github-fork-ribbon-css -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-fork-ribbon-css/0.2.0/gh-fork-ribbon.min.css" />
+
+	<link rel="stylesheet" href="https://rawgit.com/TimPietrusky/KudosPlease/master/css/kudosplease.css">
+
+	<link rel="stylesheet" href="https://veri.alicangonullu.biz/p2p/css/bootstrap.css">
+	<link rel="stylesheet" href="https://veri.alicangonullu.biz/p2p/css/main.css">
+	<link rel="canonical" href="https://veri.alicangonullu.biz/p2p/">
+	<link rel="alternate" type="application/rss+xml" title="P2P Sistem" href="https://veri.alicangonullu.biz/p2p/feed.xml">
+	
+</head>
+
+
+<body>
+<header class="site-header" role="banner">
+
+	<div class="wrapper">
+
+		
+
+	</div>
+
+</header>
+
+
+	<main class="page-content" aria-label="Content">
+		<div class="wrapper">
+			<div id="home-banner">
+</div>
+
+<hr>
+
+<form id="magnet-input">
+	<input name="torrentId" placeholder="magnet:"><button type="submit">Oynat</button>
+</form>
+
+<div id="hero">
+	<div id="output">
+		<div id="progressBar"></div>
+		<!-- The video player will be added here -->
+	</div>
+
+	<!-- Statistics -->
+	<div id="status">
+		<div>
+		<code>Deneme İçin : magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel&tr=udp%3a%2f%2ftracker.leechers-paradise.org%3a6969&tr=udp%3a%2f%2ftracker.coppersurfer.tk%3a6969&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337&tr=udp%3a%2f%2fexplodie.org%3a6969&tr=udp%3a%2f%2ftracker.empire-js.us%3a1337&tr=wss%3a%2f%2ftracker.btorrent.xyz&tr=wss%3a%2f%2ftracker.openwebtorrent.com&tr=wss%3a%2f%2ftracker.fastcast.nz&ws=https%3a%2f%2fwebtorrent.io%2ftorrents%2f</code>
+		</div>
+	<div id="share-clipboard"></div>
+	</div>
+</div>
+
+<hr>
+
+		</div>
+	</main>
+
+
+
+	<!-- jQuery -->
+	<script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+
+	<script src="https://veri.alicangonullu.biz/p2p/js/bootstrap.min.js"></script>
+
+	<!-- clipboard.js -->
+	<script src="https://veri.alicangonullu.biz/p2p/js/clipboard.min.js"></script>
+
+	<!-- kudosplease-min.js -->
+	<script src="https://veri.alicangonullu.biz/p2p/js/kudosplease-min.js"></script>
+
+	<!-- Include the latest version of WebTorrent -->
+	<script src="https://cdn.jsdelivr.net/webtorrent/latest/webtorrent.min.js"></script>
+
+	<!-- `browserify main.js -o bundle.js` -->
+	<script src="https://veri.alicangonullu.biz/p2p/js/bundle.js"></script>
+
+</body>
+
+</html>
